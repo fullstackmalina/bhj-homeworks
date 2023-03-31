5d08bd93d6f134c76118240f20bfcd09acee4676
”fullstackmalina” <malinkin@mail.ru>
2023-03-31 17:38:41 +0300

    First commit

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -1,3 +1,5 @@
+
+
 class Game {
   constructor(container) {
     this.container = container;
@@ -7,7 +9,14 @@ class Game {
 
     this.reset();
 
+    this.TIME_LIMIT = document.querySelectorAll('.symbol').length
+    this.timeLeft = this.TIME_LIMIT
+    this.timePassed = 0;
+    this.timerInterval = null;
+    this.formsTimer();
+
     this.registerEvents();
+
   }
 
   reset() {
@@ -16,24 +25,103 @@ class Game {
     this.lossElement.textContent = 0;
   }
 
-  registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
+  async registerEvents() {
+
+    document.addEventListener('keydown', event => {
+      if (this.winsElement.textContent == 0 && this.lossElement.textContent == 0 && document.querySelectorAll('.symbol')[0].innerHTML == this.currentSymbol.textContent.toLowerCase()) {
+        this.startTimer();
+      }
+      const eventLetter = event.key.toLowerCase().charCodeAt(0);
+      const screenLetter = this.currentSymbol.textContent.toLowerCase().charCodeAt(0);
+      if (screenLetter === eventLetter) {
+        this.success();
+      } else {
+        this.fail();
+      }
+    });
+  }
+
+  formsTimer() {
+    document.getElementById("app").innerHTML = `
+    <div class="base-timer">
+    <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+      <g class="base-timer__circle">
+        <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+        <path
+          id="base-timer-path-remaining"
+          stroke-dasharray="283"
+          class="base-timer__path-remaining green"
+          d="
+            M 50, 50
+            m -45, 0
+            a 45,45 0 1,0 90,0
+            a 45,45 0 1,0 -90,0
+          "
+        ></path>
+      </g>
+    </svg>
+    <span id="base-timer-label" class="base-timer__label">${this.formatTime(
+      this.timeLeft
+    )}</span>
+  </div>
+`;
+  }
+
+  startTimer() {
+    this.timerInterval = setInterval(() => {
+      this.timePassed = this.timePassed += 1;
+      this.timeLeft = this.TIME_LIMIT - this.timePassed;
+
+      document.getElementById("base-timer-label").innerHTML = this.formatTime(this.timeLeft);
+      this.setCircleDasharray();
+
+      if (this.timeLeft === 0) {
+        this.fail()
+      }
+    }, 1000);
+  }
+
+  formatTime(time) {
+    const minutes = Math.floor(time / 60);
+
+    let seconds = time % 60;
+
+    if (seconds < 10) {
+      seconds = `0${seconds}`;
+    }
+
+    return `${minutes}:${seconds}`;
+  }
+
+  onTimesUp() {
+    clearInterval(this.timerInterval);
+  }
+
+  calculateTimeFraction() {
+    const rawTimeFraction = this.timeLeft / this.TIME_LIMIT;
+    return rawTimeFraction - (1 / this.TIME_LIMIT) * (1 - rawTimeFraction);
+  }
+
+  setCircleDasharray() {
+    const circleDasharray = `${(this.calculateTimeFraction() * 283).toFixed(0)} 283`;
+    document.getElementById("base-timer-path-remaining").setAttribute("stroke-dasharray", circleDasharray);
+  }
+
+  subsequentStartTimerFunctions() {
+
+    this.TIME_LIMIT = document.querySelectorAll('.symbol').length
+    this.timeLeft = this.TIME_LIMIT;
+    this.timePassed = 0;
+    this.timerInterval = null;
+    this.formsTimer();
+    this.onTimesUp();
+    this.startTimer()
   }
 
   success() {
-    if(this.currentSymbol.classList.contains("symbol_current")) this.currentSymbol.classList.remove("symbol_current");
     this.currentSymbol.classList.add('symbol_correct');
     this.currentSymbol = this.currentSymbol.nextElementSibling;
-
     if (this.currentSymbol !== null) {
-      this.currentSymbol.classList.add('symbol_current');
       return;
     }
 
@@ -41,6 +129,9 @@ class Game {
       alert('Победа!');
       this.reset();
     }
+    this.onTimesUp();
+    this.subsequentStartTimerFunctions()
+
     this.setNewWord();
   }
 
@@ -49,6 +140,9 @@ class Game {
       alert('Вы проиграли!');
       this.reset();
     }
+    this.onTimesUp();
+    this.subsequentStartTimerFunctions()
+
     this.setNewWord();
   }
 
@@ -60,18 +154,18 @@ class Game {
 
   getWord() {
     const words = [
-        'bob',
-        'awesome',
-        'netology',
-        'hello',
-        'kitty',
-        'rock',
-        'youtube',
-        'popcorn',
-        'cinema',
-        'love',
-        'javascript'
-      ],
+      'bob',
+      'awesome',
+      'netology',
+      'hello',
+      'kitty',
+      'rock',
+      'youtube',
+      'popcorn',
+      'cinema',
+      'love',
+      'javascript'
+    ],
       index = Math.floor(Math.random() * words.length);
 
     return words[index];
@@ -81,7 +175,7 @@ class Game {
     const html = [...word]
       .map(
         (s, i) =>
-          `<span class="symbol ${i === 0 ? 'symbol_current': ''}">${s}</span>`
+          `<span class="symbol ${i === 0 ? 'symbol_current' : ''}">${s}</span>`
       )
       .join('');
     this.wordElement.innerHTML = html;
@@ -91,4 +185,3 @@ class Game {
 }
 
 new Game(document.getElementById('game'))
-
---
+++ event-object/tabs/task.js
@@ -0,0 +1,16 @@
+
+
+const tabs = document.querySelectorAll('.tab');
+const contentList = document.querySelectorAll('.tab__content');
+
+tabs.forEach(el => el.addEventListener('click', change));
+
+function change(event) {
+
+    tabs.forEach(el => el.className = 'tab');
+    event.target.classList.add('tab_active');
+    contentList.forEach(el => el.className = 'tab__content');
+    const index = Array.from(tabs).indexOf(event.target);
+    contentList[index].classList.add('tab__content_active');
+
+}
