a01e9050d9fb87443836215edfebc6ff398b6f02
”fullstackmalina” <malinkin@mail.ru>
2023-04-03 08:55:58 +0300

    first commit

---
+++ async-requests/progressbar/task.js
@@ -0,0 +1,28 @@
+const progress = document.getElementById('progress');
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const form = document.getElementById('form');
+    const formData = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
+
+    xhr.upload.onprogress = function (event) {
+        progress.value = event.loaded / event.total;
+    }
+
+    xhr.addEventListener('readystatechange', (event) => {
+        if (xhr.status === 200) {
+            xhr.abort();
+        }
+    })
+
+    // xhr.onload = function() {
+    //   if (xhr.status === 200) {
+    //     xhr.abort();
+    //   }
+    // } 
+
+    xhr.send(formData);
+});
